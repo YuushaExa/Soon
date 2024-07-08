4a5441e40dbb5327959969199c4e8891f34305b5
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-07-09 02:53:23 +0300

    Update file2.js

--- file2.js
+++ file2.js
@@ -1,9 +1,21 @@
 const reduceCollectTimeButton = document.createElement('button');
 reduceCollectTimeButton.textContent = 'Reduce Collect Time x10';
 reduceCollectTimeButton.addEventListener('click', () => {
-    mines.forEach(mine => {
-        mine.timeToCollect /= 10;
-    });
+    let index = 0;
+
+    function reduceMineCollectTime() {
+        if (index < mines.length) {
+            const currentMine = mines[index];
+            if (!currentMine.collecting) {
+                currentMine.timeToCollect /= 10;
+                index++;
+            } else {
+                setTimeout(reduceMineCollectTime, 1000);
+            }
+        }
+    }
+
+    reduceMineCollectTime();
 });
 
 document.body.appendChild(reduceCollectTimeButton);
