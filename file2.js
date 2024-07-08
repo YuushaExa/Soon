270c887a76aa0d6e818f36f3674e0a3af2cda741
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-07-09 02:47:30 +0300

    Update file2.js

--- file2.js
+++ file2.js
@@ -1,15 +1,9 @@
-function reduceCollectTime() {
-    mines.forEach(mine => {
-        mine.timeToCollect /= 10;
-    });
-}
-
-// Create a button element in the HTML for increasing collect time
 const reduceCollectTimeButton = document.createElement('button');
 reduceCollectTimeButton.textContent = 'Reduce Collect Time x10';
 reduceCollectTimeButton.addEventListener('click', () => {
-    reduceCollectTime();
+    mines.forEach(mine => {
+        mine.timeToCollect /= 10;
+    });
 });
 
-// Append the button to the body of the HTML document
 document.body.appendChild(reduceCollectTimeButton);
